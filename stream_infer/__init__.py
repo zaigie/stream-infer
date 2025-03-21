abd5b5fb9648c6236eded74e264a9613bb3bf2c4
zaigie <jokerwho@yeah.net>
2025-03-22 01:12:20 +0800

    fix: default version don't need streamlit

--- stream_infer/__init__.py
+++ stream_infer/__init__.py
@@ -2,4 +2,13 @@ __version__ = "0.4.3"
 
 from .inference import Inference
 from .player import Player
-from .streamlit_ import StreamlitApp
+
+try:
+    from .streamlit_ import StreamlitApp
+except ImportError:
+
+    class StreamlitApp:
+        def __init__(self, *args, **kwargs):
+            raise ImportError(
+                "StreamlitApp needs streamlit package support. Please use 'pip install \"stream-infer[server]\"' to install the server version."
+            )
